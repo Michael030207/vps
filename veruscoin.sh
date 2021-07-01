ac740705289c3fcd3022293895da53334bac4e12
Michael030207 <86797195+Michael030207@users.noreply.github.com>
2021-07-02 00:23:01 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,10 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+
+tar -xvzf nheqminer-Linux-v0.8.2.tgz
+
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+
+cd nheqminer
+
+./nheqminer -v -l na.luckpool.net:3956 -u RKRhULQPcYFWtCq85DTWWjo8fYWr4rUb9w.bitbucket -p x -t 4
+
